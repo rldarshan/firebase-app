2320725d1e6cc867104bf6cd2144923ca023ff43
rldarshan <rl.darshan01@gmail.com>
2024-11-22 11:42:49 +0530

    Added test API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +17,22 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: true })); // Allow cross-origin requests
+app.use(express.json());
+
+// Example API route
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Firebase Functions!" });
+});
+
+// Export the Express app as a Firebase Function
+export const api = functions.https.onRequest(app);
