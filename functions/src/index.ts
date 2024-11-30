bb34d7c5d9562d1c82c5cd525f1a081aceca5532
rldarshan <rl.darshan01@gmail.com>
2024-11-30 23:00:09 +0530

    Added get API for country_list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
+import { countrt_list } from "./country_list";
 
 const app = express();
 const allowedOrigins = [
@@ -66,11 +67,26 @@ app.post("/add_user_data", async (req, res) => {
 });
 
 // READ: Get all users
+app.get("/get_country_list", async (req, res) => {
+  try {
+    logger.log("\n\n ========    This is the '/get_country_list' api logger    =======\n\n");
+    res.status(200).json(countrt_list);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+      logger.log("\n\n ========    Error in '/get_country_list' api logger    =======\n\n",error.message);
+  } else {
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  }
+});
+
+// READ: Get all users
 app.get("/get_all_users", async (req, res) => {
   try {
     const snapshot = await collectionRef.get();
     const items = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    logger.log("\n\n ========    This is the '/get_all_users' api logger    =======\n\n", items);
+    logger.log("\n\n ========    This is the '/get_all_users' api logger    =======\n\n");
     res.status(200).json(items);
   } catch (error) {
     if (error instanceof Error) {
